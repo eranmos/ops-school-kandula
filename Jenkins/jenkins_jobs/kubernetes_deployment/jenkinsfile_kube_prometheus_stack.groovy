b242951f9bb47377b79f4e8ba9ea2bbd2ef18479
= <=>
2022-03-03 19:52:10 +0200

    updating build job

--- Jenkins/jenkins_jobs/kubernetes_deployment/jenkinsfile_kube_prometheus_stack.groovy
+++ Jenkins/jenkins_jobs/kubernetes_deployment/jenkinsfile_kube_prometheus_stack.groovy
@@ -45,11 +45,12 @@ pipeline {
                     withCredentials([file(credentialsId: 'AWS-KANDULA-Credentials', variable: 'CREDENTIALSFILE'), file(credentialsId: "${KUBECONFIG_VAR}", variable: 'KUBECONFIG')]) {
                         sh 'mkdir /home/jenkins/.aws/ && cp \$CREDENTIALSFILE /home/jenkins/.aws/credentials && chmod 640 /home/jenkins/.aws/credentials'
                         configFileProvider([configFile(fileId: 'AWS-KANDULA-config', targetLocation: '/home/jenkins/.aws/config')]) {
-                                    echo 'Going to Install Upgrade helm chart'
-                                    sh """helm upgrade ${DEPLOYMENT_NAME} ./kube-prometheus-stack --install --atomic --namespace=${NAMESPACE}"""
-                                    sh "kubectl get pods -n ${NAMESPACE}"
-                                    sh "kubectl rollout status deployment ${DEPLOYMENT_NAME} -n ${NAMESPACE}"
-                                    echo "Yoy successfully deployed kube-prometheus-stack on your Env"
+                            sleep 8000
+                            echo 'Going to Install Upgrade helm chart'
+                            sh """helm upgrade ${DEPLOYMENT_NAME} ./kube-prometheus-stack --install --atomic --namespace=${NAMESPACE}"""
+                            sh "kubectl get pods -n ${NAMESPACE}"
+                            sh "kubectl rollout status deployment ${DEPLOYMENT_NAME} -n ${NAMESPACE}"
+                            echo "Yoy successfully deployed kube-prometheus-stack on your Env"
                         }
                     }
                 }
