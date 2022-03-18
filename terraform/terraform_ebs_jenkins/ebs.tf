c5eafc0f35f47e8408b479a6810a3362f82528ae
= <=>
2022-03-18 17:08:51 +0200

    Terraform config change for EBS resource. I am adding more 4 ebs volumes to support my monitoring solution fort ELK & Prometheus

--- terraform/terraform_ebs_jenkins/ebs.tf
+++ terraform/terraform_ebs_jenkins/ebs.tf
@@ -17,3 +17,43 @@ resource "aws_ebs_volume" "jenkins_master_data_volume" {
   }
 
 }
+
+resource "aws_ebs_volume" "elasticsearch_data_volume" {
+  count                     = var.ebs_volume_count_elasticsearch
+  availability_zone         = var.ebs_availability_zone
+  multi_attach_enabled      = var.ebs_multi_attach_enabled
+  encrypted                 = var.ebs_encrypted
+  size                      = var.ebs_volume_size_elasticsearch
+  type                      = var.ebs_volume_type
+
+
+  tags = {
+    Name = "elasticsearch_data_volume${count.index+1}"
+    Owner                 = local.eran_tags.owner
+    Environment_Name      = local.eran_tags.environment_name
+    Project_Name          = local.eran_tags.project_name
+    "tr:resource-owner"   = var.asset_owner
+    "tr:environment-type" = var.environment
+    "tr:application-asset-insight-id" = var.asset_id
+  }
+}
+
+resource "aws_ebs_volume" "prometheus_data_volume" {
+  count                     = var.ebs_volume_count_prometheus
+  availability_zone         = var.ebs_availability_zone
+  multi_attach_enabled      = var.ebs_multi_attach_enabled
+  encrypted                 = var.ebs_encrypted
+  size                      = var.ebs_volume_size_prometheus
+  type                      = var.ebs_volume_type
+
+
+  tags = {
+    Name = "prometheus_data_volume${count.index+1}"
+    Owner                 = local.eran_tags.owner
+    Environment_Name      = local.eran_tags.environment_name
+    Project_Name          = local.eran_tags.project_name
+    "tr:resource-owner"   = var.asset_owner
+    "tr:environment-type" = var.environment
+    "tr:application-asset-insight-id" = var.asset_id
+  }
+}
--- terraform/terraform_ebs_jenkins/terraform.tfvars
+++ terraform/terraform_ebs_jenkins/terraform.tfvars
@@ -4,13 +4,21 @@ aws_cli_profile = "tr-tms"
 ## AWS account data
 aws_region      = "us-east-1"
 
-############# EBS Volume config #############
+############# Jenkins EBS Volume config #############
 ebs_encrypted                 = "true"
 ebs_volume_type               = "gp2"
 ebs_volume_size               = "16"
 ebs_multi_attach_enabled      = "false"
 ebs_device_name               = "/dev/sdh"
 ebs_availability_zone         = "us-east-1a"
+
+############# elasticsearch EBS Volume config #######
+ebs_volume_count_elasticsearch = "3"
+ebs_volume_size_elasticsearch  = "10"
+
+############# prometheus EBS Volume config #######
+ebs_volume_count_prometheus = "1"
+ebs_volume_size_prometheus  = "10"
 
 #############  Tags Related  #############
 environment_name = "ops-school"
