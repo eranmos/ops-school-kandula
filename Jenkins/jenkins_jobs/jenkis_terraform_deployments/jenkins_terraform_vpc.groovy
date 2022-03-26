3938158054463160df001860adcc870a51c5846c
= <=>
2022-03-26 22:39:55 +0300

    updating jenkins slave root storage size

--- Jenkins/jenkins_jobs/jenkis_terraform_deployments/jenkins_terraform_vpc.groovy
+++ Jenkins/jenkins_jobs/jenkis_terraform_deployments/jenkins_terraform_vpc.groovy
@@ -29,10 +29,10 @@ pipeline {
                             echo 'Going to run terraform plan to see that changes that you done'
                             sh """wget https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh && chmod 755 install.sh"""
                             sh './install.sh -b $(pwd)/.bin'
-                            sh """CUSTOMBIN=$(pwd)/.bin"""
-                            sh """export PATH=$CUSTOMBIN:$PATH"""
-                            sh """$CUSTOMBIN/tfswitch -b $CUSTOMBIN/terraform 1.1.7"""
-                            sh """terraform -v"""
+                            sh 'CUSTOMBIN=$(pwd)/.bin'
+                            sh 'export PATH=$CUSTOMBIN:$PATH'
+                            sh '$CUSTOMBIN/tfswitch -b $CUSTOMBIN/terraform 1.1.7'
+                            sh 'terraform -v'
                             echo "Yoy successfully run terraform plan to see your changes via terraform"
                     }
                 }
