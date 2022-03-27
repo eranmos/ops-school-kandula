6aab1c7a98b75b3b78f9d1073f36968c4efa5bb0
= <=>
2022-03-27 15:03:47 +0300

    creating jenkins job for terraform deployments

---
+++ Jenkins/jenkins_jobs/jenkis_terraform_deployments/jenkins_terraform_build_kandula_env.groovy
@@ -0,0 +1,54 @@
+properties([
+  parameters([
+    choice(name: 'TERRAFORM',choices: ['plan', 'apply', 'destroy'],description: 'Terraform: plan, apply or destroy')
+  ])
+])
+
+pipeline {
+
+    agent {
+        label 'docker-ubuntu'
+    }
+    tools {
+        terraform "Terraform_1.1.2"
+      }
+
+    options {
+        ansiColor('xterm')
+        buildDiscarder(logRotator(numToKeepStr: '25'))
+        disableConcurrentBuilds()
+        timestamps()
+        timeout(60)
+    }
+
+    stages {
+        stage('Terrafrom Plan - Creating Kandula Env on AWS')
+                stage('Creating VPC for Kandula') {
+                    steps {
+                    build job:'terraform-vpc', parameters: [choice(name: 'TERRAFORM',choices: ['plan', 'apply', 'destroy'],description: 'Terraform: plan, apply or destroy')]
+                    }
+                }
+                stage('Creating Jenkins Servers') {
+                    steps {
+                    build job:'terraform-vpc', parameters: [choice(name: 'TERRAFORM',choices: ['plan', 'apply', 'destroy'],description: 'Terraform: plan, apply or destroy')]
+                    }
+                }
+        }
+    }
+
+ post {
+        success {
+            slackSend(
+                color: 'good',
+                message: "SUCCESSFUL: Job ${env.JOB_NAME} - #${env.BUILD_NUMBER} - (<${env.BUILD_URL}|Open>)"
+            )
+        }
+        failure {
+            echo "BUILD FAILED \u274C"
+            slackSend(
+                color: 'danger',
+                message: "FAILED: Job ${env.JOB_NAME} - #${env.BUILD_NUMBER} - (<${env.BUILD_URL}|Open>)"
+            )
+        }
+    }
+}
