8eacdffb0c3cfcdabd98d694ffa0187c88ab2864
= <=>
2022-03-27 15:05:25 +0300

    creating jenkins job for terraform deployments

--- Jenkins/jenkins_jobs/jenkis_terraform_deployments/jenkins_terraform_build_kandula_env.groovy
+++ Jenkins/jenkins_jobs/jenkis_terraform_deployments/jenkins_terraform_build_kandula_env.groovy
@@ -22,7 +22,7 @@ pipeline {
     }
 
     stages {
-        stage('Terrafrom Plan - Creating Kandula Env on AWS')
+        stage('Terrafrom Plan - Creating Kandula Env on AWS') {
                 stage('Creating VPC for Kandula') {
                     steps {
                     build job:'terraform-vpc', parameters: [choice(name: 'TERRAFORM',choices: ['plan', 'apply', 'destroy'],description: 'Terraform: plan, apply or destroy')]
